44fa43fda2724c3cdb807b03a3d02e3806680642
Lucie <monjotlucie@gmail.com>
2026-02-11 09:44:23 +0100

    Ajout de plateformes simples qui disparaissent selon le mode

---
+++ sae-601/Script/Map/static_platform_night.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+@onready var collision: CollisionShape2D = $CollisionShape2D
+@onready var visual: Node = $ColorRect 
+
+func _ready():
+	GameState.day_night_changed.connect(_on_day_night_changed)
+	_on_day_night_changed(GameState.is_night)
+
+func _on_day_night_changed(night: bool):
+	if night:
+		collision.set_deferred("disabled", false)
+	else:
+		collision.set_deferred("disabled", true)
+	
