01699d9a19ee179383a2a9d1ba574d8a15dacc4b
Lucie <monjotlucie@gmail.com>
2026-02-23 10:19:07 +0100

    Les sprike qui fonctionne bien chacun tue le player selon jour/nuit

--- sae-601/Script/Map/sprike_day.gd
+++ sae-601/Script/Map/sprike_day.gd
@@ -5,5 +5,5 @@ func _ready() -> void:
 		body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node) -> void:
-	if body is Player:
+	if body is Player and not GameState.is_night:
 		body.die()
